59806d39d2a06fd79432449f972c2ef0a28ec2de
Sheriff Amzat <kunmiamzat1@gmail.com>
2023-07-11 22:44:07 +0100

    Initial commit

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+require('dotenv').config();
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +22,35 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", function (req, res) {
+  let unixTimeStamp;
+  let utcFormattedDate;
+  const dateValue = req.params.date;
 
+  if (dateValue) {
+    const parsedDate = isNaN(dateValue) ? new Date(dateValue) : new Date(parseInt(dateValue));
+    const isValidDate = !isNaN(parsedDate);
+
+    if (isValidDate) {
+      unixTimeStamp = parsedDate.getTime();
+      utcFormattedDate = parsedDate.toUTCString();
+    } else {
+      res.json({ error: "Invalid Date" });
+      return;
+    }
+  } else {
+    unixTimeStamp = Date.now();
+    utcFormattedDate = new Date().toUTCString();
+  }
+
+  res.json({
+    unix: unixTimeStamp,
+    utc: utcFormattedDate
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
